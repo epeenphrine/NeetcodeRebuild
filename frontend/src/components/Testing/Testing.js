467f7e4d765d224d501077726cba3780bced8191
dong <donghyunwon610@gmail.com>
2020-05-31 21:30:04 -0400

    filter function

--- frontend/src/components/Testing/Testing.js
+++ frontend/src/components/Testing/Testing.js
@@ -3,13 +3,19 @@ import React, {useState, useEffect} from 'react'
 export default function Testing() {
     const [api, setApi] = useState(
         {
-            data: {}
+            data: [] 
         }
     )
 
-    async function getAPI() {
-        const res = await fetch('http://192.168.86.31:1500/proxy/project')
-            .then(resp => resp.json()).catch((err) => console.log(err))
+     function getAPI() {
+        const res = [
+            "united states",
+            "china",
+            "korea",
+            "japan"
+        ] 
+
+        console.log(res)
         setApi({
             data: res
         })
@@ -18,13 +24,20 @@ export default function Testing() {
     useEffect(() => {
         getAPI()
     }, [])
+
     const items = api.data.map(item => {
         return (
-            <h1>{item.project}</h1>
+            <h1>{item}</h1>
         )
     })
+
+    const filter = api.data.filter( item => item.includes("chin")).map(filtereditem => (
+    <h1>{filtereditem}</h1>
+    ))
+
     return (
         <div>
+            {filter}
         </div>
     )
 }
